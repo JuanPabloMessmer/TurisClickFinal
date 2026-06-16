7003569f07a7786db2c40f3b3aa33014deb5077e
unknown <jpmessmer1029@gmail.com>
2026-06-15 23:53:47 -0400

    Initial ASP.net core project structure

---
+++ Modules/Roles/Role.cs
@@ -0,0 +1,10 @@
+﻿namespace TurisClick.Api.Modules.Roles;
+
+public class Role
+{
+    public int IdRol { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public string? Descripcion { get; set; }
+
+    public ICollection<Users.User> Usuarios { get; set; } = new List<Users.User>();
+}
---
+++ Modules/Users/User.cs
@@ -0,0 +1,19 @@
+﻿using TurisClick.Api.Modules.Roles;
+
+namespace TurisClick.Api.Modules.Users;
+
+public class User
+{
+    public int IdUsuario { get; set; }
+    public int IdRol { get; set; }
+
+    public string Nombre { get; set; } = string.Empty;
+    public string? Apellido { get; set; }
+    public string Correo { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string? Telefono { get; set; }
+    public string Estado { get; set; } = "ACTIVO";
+    public DateTime FechaRegistro { get; set; } = DateTime.UtcNow;
+
+    public Role Rol { get; set; } = null!;
+}
